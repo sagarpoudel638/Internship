3f289bd225a834aef27fad4df8a8f3e46b898692
Sagar Poudel <sagarpoudel638@gmail.com>
2021-03-11 21:45:43 +0545

    Create LiveSearchCustomer.php

---
+++ RestroPos/app/Http/Controllers/LiveSearchCustomer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LiveSearchCustomer extends Controller
+{
+    function searchAction(Request $request)
+    {
+        
+     if($request->ajax())
+     {
+      $result = '';
+     
+      $queries = $request->get('query');
+      if($queries != '')
+      {
+       $searchcustomerdata = DB::table('customer')
+         ->where('firstname', 'like', '%'.$queries.'%')
+         ->orderBy('id', 'desc')
+         ->get();
+         
+         
+      }
+     
+
+      $total_row = $searchcustomerdata->count();
+
+      if($total_row > 0)
+      {
+
+       foreach($searchcustomerdata as $row)
+       {
+
+        $result .= '
+        <tr id="livesearchcustomer">
+        
+        
+        <td>'.$row->firstname.'</td>
+        <td>'.$row->lastname.'</td>
+        <td>'.$row->phonenumber.'</td>
+         
+         <td><a href="" class="btn btn-primary btn-xs">Add</a></td>
+         
+        </tr>
+        ';
+       }
+      }
+      else
+      {
+       $result = '
+       <tr>
+        <td align="center" colspan="5">No Data Found</td>
+       </tr>
+       '; 
+      }
+      $searchcustomerdata = array(
+       'table_data_customer'  => $result,
+       
+      );
+
+      echo json_encode($searchcustomerdata);
+     }
+    }
+}
