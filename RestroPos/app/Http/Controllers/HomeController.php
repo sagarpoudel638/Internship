0a406806393a46d71186fe7521b5a6667fb24b81
Sagar Poudel <sagarpoudel638@gmail.com>
2021-03-11 21:45:26 +0545

    Update HomeController.php

--- RestroPos/app/Http/Controllers/HomeController.php
+++ RestroPos/app/Http/Controllers/HomeController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\item;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    function home()
-    {
-        
-        return view('home');
+    function AddData(Request $request){
+        $id= $request->user_id;
+        $AddItem = item::findOrFail($id);
     }
 }
